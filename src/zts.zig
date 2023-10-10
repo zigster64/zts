b87f389bef3f652ef82be70eda68c0ae8a0617eb
𒀳 Scribe of the Ziggurat𒅆𒂍𒉪 <steve@raffmail.net>
2023-10-10 11:13:20 +1000

    foobar and all tests work

--- src/zts.zig
+++ src/zts.zig
@@ -15,16 +15,9 @@ fn template(comptime str: []const u8) type {
 
     // empty strings, or strings that dont start with a .directive - just map the whole string to .all and return early
     if (str.len < 1 or str[0] != '.') {
-        var fields: [2]std.builtin.Type.StructField = undefined;
+        var fields: [1]std.builtin.Type.StructField = undefined;
         fields[0] = .{
             .name = "all",
-            .type = [str.len]u8,
-            .is_comptime = true,
-            .alignment = 0,
-            .default_value = str[0..],
-        };
-        fields[0] = .{
-            .name = "raw",
             .type = [str.len]u8,
             .is_comptime = true,
             .alignment = 0,
@@ -112,8 +105,11 @@ fn template(comptime str: []const u8) type {
                     '\n' => {
                         // found a new directive - we need to patch the value of the previous content then
                         if (field_num > 1) {
-                            // @compileLog("patching", field_num - 1, content_start, directive_start);
-                            fields[field_num - 1].default_value = str[content_start..directive_start];
+                            // @compileLog("patching", field_num - 1, content_start, directive_start - 1);
+                            var adjusted_len = directive_start - content_start;
+                            fields[field_num - 1].type = [adjusted_len]u8;
+                            fields[field_num - 1].default_value = str[content_start .. directive_start - 1];
+                            // @compileLog("patched previous to", fields[field_num - 1]);
                         }
                         const dname = str[directive_start + 1 .. index];
                         const dlen = str.len - index;
@@ -153,17 +149,17 @@ fn template(comptime str: []const u8) type {
     });
 }
 
-test "all text" {
+test "all" {
     const t = embed("testdata/all.txt");
     inline for (@typeInfo(t).Struct.fields, 0..) |f, i| {
         std.debug.print("all.txt has field {} name {s} type {}\n", .{ i, f.name, f.type });
     }
     const data = t{};
     std.debug.print("Whole contents of all.txt is:\n{s}\n", .{data.all});
-    try std.testing.expectEqual(data.all.len, 58);
+    try std.testing.expectEqual(57, data.all.len);
 }
 
-test "foo bar" {
+test "foobar" {
     const t = embed("testdata/foobar.txt");
     inline for (@typeInfo(t).Struct.fields, 0..) |f, i| {
         std.debug.print("foobar.txt has field {} name {s} type {}'\n", .{ i, f.name, f.type });
@@ -171,9 +167,9 @@ test "foo bar" {
     const data = t{};
 
     std.debug.print("Whole contents of foobar.txt is:\n---------------\n{s}\n---------------\n", .{data.all});
-    std.debug.print("foo: '{s}'\n", .{data.foo});
-    std.debug.print("bar: '{s}'\n", .{data.bar});
-    try std.testing.expectEqual(data.all.len, 52);
-    try std.testing.expectEqualStrings(data.foo, "I like the daytime");
-    try std.testing.expectEqualStrings(data.bar, "I prefer the nighttime");
+    std.debug.print("\nfoo: '{s}'\n", .{data.foo});
+    std.debug.print("\nbar: '{s}'\n", .{data.bar});
+    try std.testing.expectEqual(52, data.all.len);
+    try std.testing.expectEqual(19, data.foo.len);
+    try std.testing.expectEqual(24, data.bar.len);
 }
