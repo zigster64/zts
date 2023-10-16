6ef43890ee6387258c13710f0269c3322d96dc06
𒀳 Scribe of the Ziggurat𒅆𒂍𒉪 <steve@raffmail.net>
2023-10-16 23:58:04 +1000

    setevalbranch to 1m

--- src/bench.zig
+++ src/bench.zig
@@ -2,8 +2,8 @@ const std = @import("std");
 const zts = @import("zts.zig");
 
 pub fn main() !void {
-    std.debug.print("Do 1million runs of passing data through HTML template", .{});
-    var out = std.getStdOut().writer(); // pipe this to /dev/null and time it
+    std.debug.print("Do 100k runs of passing data through HTML template\n", .{});
+    var out = std.io.getStdOut().writer(); // pipe this to /dev/null and time it
 
     const tmpl = @embedFile("testdata/customer_details.html");
 
@@ -26,17 +26,24 @@ pub fn main() !void {
         .{ .date = "2023-10-24", .details = "Chocolate Milkshake", .amount = 80.99 },
     };
 
-    try zts.printHeader(tmpl, .{}, out);
+    const t1 = std.time.microTimestamp();
+    for (0..100_000) |i| {
+        _ = i;
+        try zts.printHeader(tmpl, .{}, out);
 
-    // print the customer details
-    try zts.printSection(tmpl, "customer_details", customer, out);
+        // print the customer details
+        try zts.printSection(tmpl, "customer_details", customer, out);
 
-    // print a table of customer invoices
-    try zts.printSection(tmpl, "invoice_table", .{}, out);
-    var total: f32 = 0.0;
-    inline for (invoices) |inv| {
-        try zts.printSection(tmpl, "invoice_row", inv, out);
-        total += inv.amount;
+        // print a table of customer invoices
+        try zts.printSection(tmpl, "invoice_table", .{}, out);
+        var total: f32 = 0.0;
+        inline for (invoices) |inv| {
+            try zts.printSection(tmpl, "invoice_row", inv, out);
+            total += inv.amount;
+        }
+        try zts.printSection(tmpl, "invoice_total", .{ .total = total }, out);
     }
-    try zts.printSection(tmpl, "invoice_total", .{ .total = total }, out);
+    const t2 = std.time.microTimestamp();
+    const te = t2 - t1;
+    std.debug.print("Done in {d}us .. or {d}us per template\n", .{ te, @divFloor(te, 100_000) });
 }
